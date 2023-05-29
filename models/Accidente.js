7452040b8c2720c8fc1f59b13cd7369621097754
ivyncm <ivancantalejo21@hotmail.com>
2023-05-29 16:24:42 +0200

    Cambio a localhost

--- models/Accidente.js
+++ models/Accidente.js
@@ -6,7 +6,7 @@ var AccidenteSchema = new Schema({
   fecha: String,
   hora: String,
   localizacion: String,
-  numero: Number,
+  numero: String,
   cod_distrito: Number,
   distrito: String,
   tipo_accidente: String,
@@ -15,12 +15,12 @@ var AccidenteSchema = new Schema({
   tipo_persona: String,
   rango_edad: String,
   sexo: String,
-  cod_lesividad: Number,
+  cod_lesividad: String,
   lesividad: String,
-  coordenada_x_utm: Number,
-  coordenada_y_utm: Number,
-  positiva_alcohol: Boolean,
-  positiva_droga: Boolean
+  coordenada_x_utm: String,
+  coordenada_y_utm: String,
+  positiva_alcohol: String,
+  positiva_droga: String
 });
 
 module.exports = mongoose.model('Accidente', AccidenteSchema);
